aa6db1d419694b8d556c96888f0e128a3d7aba03
RTEMS User <rtems@localhost.localdomain>
2014-02-12 09:14:14 -0600

    可以往库中增加头文件和c文件

--- c/src/lib/libbsp/arm/stm32f4/f_include/stm32f4xx.h
+++ c/src/lib/libbsp/arm/stm32f4/f_include/stm32f4xx.h
@@ -6983,6 +6983,28 @@ typedef struct
 
 #define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
 
+/* Uncomment the line below to expanse the "assert_param" macro in the 
+   Standard Peripheral Library drivers code */
+#define USE_FULL_ASSERT    0
+
+/* Exported macro ------------------------------------------------------------*/
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  The assert_param macro is used for function's parameters check.
+  * @param  expr: If expr is false, it calls assert_failed function
+  *   which reports the name of the source file and the source
+  *   line number of the call that failed. 
+  *   If expr is true, it returns no value.
+  * @retval None
+  */
+  #define assert_param(expr) ((void)0)
+/* Exported functions ------------------------------------------------------- */
+  void assert_failed(uint8_t* file, uint32_t line);
+#else
+  #define assert_param(expr) ((void)0)
+#endif /* USE_FULL_ASSERT */
+
 /**
   * @}
   */
